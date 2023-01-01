b3d1d2d56038711d8835e0610c6ca6ded0ba772d
baba-s <shotababa1206@gmail.com>
2023-01-01 13:41:30 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Kogane.Padding
+﻿# Kogane Padding
+
+padding を管理する構造体
+
+## 使用例
+
+```cs
+using Kogane;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    [SerializeField] private Padding m_padding;
+
+    private void Start()
+    {
+        Debug.Log( m_padding.Left );
+        Debug.Log( m_padding.Top );
+        Debug.Log( m_padding.Right );
+        Debug.Log( m_padding.Bottom );
+    }
+}
+```
---
+++ Runtime/Padding.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kogane
+{
+    /// <summary>
+    /// padding を管理する構造体
+    /// </summary>
+    [Serializable]
+    public struct Padding
+    {
+        //================================================================================
+        // 変数(SerializeField)
+        //================================================================================
+        [SerializeField] private float m_left;
+        [SerializeField] private float m_right;
+        [SerializeField] private float m_bottom;
+        [SerializeField] private float m_top;
+
+        //================================================================================
+        // プロパティ
+        //================================================================================
+        public float Left   => m_left;
+        public float Right  => m_right;
+        public float Bottom => m_bottom;
+        public float Top    => m_top;
+
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Padding
+        (
+            float left,
+            float right,
+            float bottom,
+            float top
+        )
+        {
+            m_left   = left;
+            m_right  = right;
+            m_bottom = bottom;
+            m_top    = top;
+        }
+
+        /// <summary>
+        /// JSON 形式の文字列に変換して返します
+        /// </summary>
+        public override string ToString()
+        {
+            return JsonUtility.ToJson( this, true );
+        }
+    }
+}
